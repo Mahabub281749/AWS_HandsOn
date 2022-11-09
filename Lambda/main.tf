05b677703bdb9d5749f3c76d7e360d65037776cf
Mahabub <mahabubhasan.austbd@gmail.com>
2022-11-09 13:32:29 +0200

    Lambda Function

---
+++ Lambda/main.tf
@@ -0,0 +1,47 @@
+terraform {
+  required_version = ">=1.0.0"
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "4.29.0"
+    }
+  }
+}
+
+provider "aws" {
+  region     = "eu-central-1"
+  access_key = ""
+  secret_key = ""
+}
+
+resource "aws_iam_role" "iam_for_lambda" {
+  name = "iam_for_lambda"
+
+  assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": "sts:AssumeRole",
+      "Principal": {
+        "Service": "lambda.amazonaws.com"
+      },
+      "Effect": "Allow",
+      "Sid": ""
+    }
+  ]
+}
+EOF
+}
+
+resource "aws_lambda_function" "test_lambda" {
+ 
+  filename      = "lambda_function_payload.zip"
+  function_name = "lambda_function_name"
+  role          = aws_iam_role.iam_for_lambda.arn
+  handler       = "lambda_function.py"
+
+  runtime = "python3.7"
+  architectures = ["x86_64"]
+
+}
